c0990f08112a76ae3025c90a3a965b83ea782a59
Vladislav Cherkasheninov <vladislav.cherkasheninov@taiste.com>
2024-09-15 17:45:23 +0300

    Mathematical logic of multiplier

--- app/src/main/java/com/vladisc/unitconverter/Result.kt
+++ app/src/main/java/com/vladisc/unitconverter/Result.kt
@@ -3,48 +3,28 @@ package com.vladisc.unitconverter
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-
-fun setMultiplier(unit1: Unit?, unit2: Unit?): Double? {
-    return when {
-        unit1 === Unit.MILLIMETERS && unit2 === Unit.CENTIMETERS -> 0.01
-        unit1 === Unit.MILLIMETERS && unit2 === Unit.METERS -> 0.0001
-        unit1 === Unit.MILLIMETERS && unit2 === Unit.FEET -> 0.00328084
-        unit1 === Unit.CENTIMETERS && unit2 === Unit.MILLIMETERS -> 10.00
-        unit1 === Unit.CENTIMETERS && unit2 === Unit.METERS -> 0.001
-        unit1 === Unit.CENTIMETERS && unit2 === Unit.FEET -> 0.0328084
-        unit1 === Unit.METERS && unit2 === Unit.CENTIMETERS -> 100.00
-        unit1 === Unit.METERS && unit2 === Unit.MILLIMETERS -> 1000.00
-        unit1 === Unit.METERS && unit2 === Unit.FEET -> 0.328084
-        unit1 === Unit.FEET && unit2 === Unit.MILLIMETERS -> 304.8
-        unit1 === Unit.FEET && unit2 === Unit.CENTIMETERS -> 30.48
-        unit1 === Unit.FEET && unit2 === Unit.METERS -> 0.3048
-        (unit1 != null && unit2 != null) && (unit1 === unit2) -> 1.00
-        else -> null
-    }
-}
-
+import androidx.compose.runtime.setValue
 
 @Composable
 fun Result(unit1: Unit?, unit2: Unit?, input: String?) {
-    val multiplier = remember { mutableStateOf<Double?>(null) }
+    var multiplier by remember { mutableStateOf<Double?>(null) }
 
-    LaunchedEffect(unit1, unit2, input) {
-        if (input != null) {
-            if (input != "") {
-                multiplier.value = setMultiplier(unit1, unit2)
-            }
-        }
+    LaunchedEffect(unit1, unit2) {
+        unit1 ?: return@LaunchedEffect
+        unit2 ?: return@LaunchedEffect
+        1.0 / unit1.multiplier * unit2.multiplier
+        multiplier = 1.0 / unit1.multiplier * unit2.multiplier
     }
 
-    if (multiplier.value != null) {
-        val result =
-            (multiplier.value!! * (input?.toDouble() ?: 1.0)).toBigDecimal().stripTrailingZeros()
-                .toPlainString()
-        Text(
-            text = "Result: $result"
-        )
-    }
+    val num = input?.takeUnless { it.isBlank() }?.toBigDecimalOrNull() ?: return
+    val mul = multiplier?.toBigDecimal() ?: return
+
+    val result = (num * mul).stripTrailingZeros().toPlainString()
+    Text(
+        text = "Result: $result"
+    )
 
 }
--- app/src/main/java/com/vladisc/unitconverter/Unit.kt
+++ app/src/main/java/com/vladisc/unitconverter/Unit.kt
@@ -1,10 +1,10 @@
 package com.vladisc.unitconverter
 
-enum class Unit(private val displayName: String) {
-    MILLIMETERS("mm"),
-    CENTIMETERS("cm"),
-    METERS("m"),
-    FEET("feet");
+enum class Unit(private val displayName: String, val multiplier: Double) {
+    MILLIMETERS("mm", 0.001),
+    CENTIMETERS("cm", 0.01),
+    METERS("m", 1.0),
+    FEET("feet", 0.328084);
 
     override fun toString(): String {
         return displayName
